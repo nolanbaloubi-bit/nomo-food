75ada36d9c675221e2729b1ec411cf8b2ed19330
Nolan <nolanbaloubi@gmail.com>
2026-07-11 05:52:00 +0100

    fin du menu début des bugs d'acceuil

--- menu.js
+++ menu.js
@@ -21,3 +21,32 @@ links.forEach(link => {
     }
 
 });
+
+// ================= MENU TABS =================
+
+const tabs = document.querySelectorAll(".menu-tabs a");
+const categories = document.querySelectorAll(".menu-category");
+
+tabs.forEach(tab => {
+
+    tab.addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        tabs.forEach(t => t.classList.remove("active"));
+
+        this.classList.add("active");
+
+        const target = this.dataset.target;
+
+        categories.forEach(category => {
+
+            category.classList.remove("active");
+
+        });
+
+        document.getElementById(target).classList.add("active");
+
+    });
+
+});
